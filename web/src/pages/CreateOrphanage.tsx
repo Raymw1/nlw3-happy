9b88a78014756c97cca1a482f23766959ac3d831
Rayan Wilbert <rayan.rmw@gmail.com>
2022-08-25 00:26:13 -0300

    Web: Mark click location on form page

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import React, { useState } from "react";
+import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet';
 
 import { FiPlus } from "react-icons/fi";
 
@@ -8,6 +8,22 @@ import Sidebar from "../components/Sidebar";
 import mapIcon from "../utils/mapIcon";
 
 export default function CreateOrphanage() {
+  const [markerPosition, setMarkerPosition] = useState({ latitude: 0, longitude: 0 });
+
+  function Markers() {
+    const map = useMap();
+    map.on('click', function(e) {
+      const { lat, lng } = e.latlng;
+      setMarkerPosition({ latitude: lat, longitude: lng });
+    })
+    return (
+      markerPosition.longitude !== 0
+      ? <Marker interactive={false} icon={mapIcon} position={[markerPosition.latitude, markerPosition.longitude]} />
+      : null
+    );
+  }
+  
+
   return (
     <div id="page-create-orphanage">
       <Sidebar />
@@ -20,12 +36,13 @@ export default function CreateOrphanage() {
               center={[-27.2092052,-49.6401092]} 
               style={{ width: '100%', height: 280 }}
               zoom={15}
+              
             >
               <TileLayer 
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
-
-              <Marker interactive={false} icon={mapIcon} position={[-27.2092052,-49.6401092]} />
+              <Markers />
+              {/* <Marker interactive={false} icon={mapIcon} position={[-27.2092052,-49.6401092]} /> */}
             </MapContainer>
 
             <div className="input-block">
